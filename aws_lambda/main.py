d8fb7ac42451debf842033494a24c140198afd63
Yoshiki <rrrtcdish2@gmail.com>
2018-06-28 00:38:52 +0900

    Delete event.json & Change DB Access method & Support multi user

--- aws_lambda/main.py
+++ aws_lambda/main.py
@@ -11,19 +11,11 @@ from helper import DecimalEncoder
 
 # --------------- Main handler ------------------
 def lambda_handler(event, context):
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
-
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
-
     if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
+        on_session_started(event['session'])
 
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
+        return on_launch(event['session'])
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
@@ -31,21 +23,15 @@ def lambda_handler(event, context):
 
 
 # --------------- DB Put Query Access ------------------
-def db_access(task_name, time):
+
+def db_access(user_id, task_name, time):
     today = str(datetime.date.today())
     time = int(time)
 
     # TODO ユーザの確認 & 例外処理
-    response = get_json_from_db("yoshiki")
-
-    # dynamoDBの値はユニコード化されて送られてくるので変換が必要
-    for i in response[u'Items']:
-        json.dumps(i, cls=DecimalEncoder)
-    # DBの値はItemsの0番目に格納されている
-    response = response['Items'][0]
+    response = get_json_from_db(user_id)
 
     print("##### db response debug #####")
-    print(type(response))
     print(response)
     print("#################")
 
@@ -114,35 +100,40 @@ def put_json_to_db(json):
 def get_json_from_db(user_id):
     dynamoDB = boto3.resource("dynamodb")
     table = dynamoDB.Table("time_management")
+    key = str(user_id)
 
     # DynamoDBへのGet処理実行
-    query_data = table.query(
-        KeyConditionExpression=Key("user_id").eq(user_id),
-        Limit=1
-    )
-    return query_data
+    try:
+        query_data = table.query(
+            KeyConditionExpression=Key("user_id").eq(key),
+            Limit=1
+        )
+    except OSError:
+        init_json = {
+            "task": [],
+            "user_id": key
+        }
+        put_json_to_db(init_json)
+        return init_json
 
-
-def get_task_time_from_db(target, date = None, user_id = "yoshiki"):
-    dynamoDB = boto3.resource("dynamodb")
-    table = dynamoDB.Table("time_management")
-
-    # DynamoDBへのGet処理実行
-    query_data = table.query(
-        KeyConditionExpression=Key("user_id").eq(user_id),
-        Limit=1
-    )
-
-    return query_data
-
-
-def get_time_by_target(target, target_date = None):
-    response = get_json_from_db("yoshiki")
     # dynamoDBの値はユニコード化されて送られてくるので変換が必要
-    for i in response[u'Items']:
+    for i in query_data[u'Items']:
         json.dumps(i, cls=DecimalEncoder)
+
+    if query_data['Count'] == 0:
+        init_json = {
+            "task": [],
+            "user_id": str(user_id)
+        }
+        put_json_to_db(init_json)
+        return init_json
+
     # DBの値はItemsの0番目に格納されている
-    response = response['Items'][0]
+    return query_data['Items'][0]
+
+
+def get_time_by_target(user_id, target, target_date=None):
+    response = get_json_from_db(user_id)
 
     # 今週だった場合
     target_date_array = []
@@ -208,12 +199,11 @@ def build_response(session_attributes, speechlet_response):
 def get_welcome_response():
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "ようこそ時間管理アプリへ" \
-                    "あなたの登録したいタスクを話しかけてみてください。" \
-                    "例えば、こんなふうに「時間管理アプリを開いて、ゆーちゅーぶの時間を記録して」"
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = "もう一度話しかけてみてください。"
+    speech_output = u"ようこそ時間管理アプリへ" \
+                    u"あなたの登録したいタスクを話しかけてみてください。" \
+                    u"例えば、こんなふうに「時間管理アプリを開いて、ゆーちゅーぶの時間を記録して」"
+
+    reprompt_text = u"もう一度話しかけてみてください。"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -221,9 +211,8 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
-    # Setting this to true ends the session and exits the skill.
+    speech_output = u"時間管理アプリをお使いいただきありがとうございます。" \
+                    u"またご利用ください！"
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
@@ -249,13 +238,13 @@ def set_task_in_session(intent, session):
     if 'target' in intent['slots']:
         target = intent['slots']['target']['value']
         session_attributes = create_task_attributes(target)
-        speech_output = target + "の時間を記録します。何時間ですか？"
-        reprompt_text = "何時間記録しますか？"
+        speech_output = target + u"の時間を記録します。何時間ですか？"
+        reprompt_text = u"何時間記録しますか？"
 
     else:
-        speech_output = "もう一度タスク名をはっきりと話してみてください。" \
-                        "例えば　「ゆーちゅーぶの時間を記録して」　というふうに話してみてください"
-        reprompt_text = "もう一度話してみてください"
+        speech_output = u"もう一度タスク名をはっきりと話してみてください。" \
+                        u"例えば　「ゆーちゅーぶの時間を記録して」　というふうに話してみてください"
+        reprompt_text = u"もう一度話してみてください"
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -275,19 +264,20 @@ def set_time_in_session(intent, session):
         session_attributes = session['attributes']
         task_name = session['attributes']['taskName']
         should_end_session = False
-        speech_output = task_name + 'の時間を' + time + '時間記録しました。'
-        db_access(task_name, time)
+        speech_output = task_name + u'の時間を' + time + u'時間記録しました。'
+        user_id = session['user']['userId']
+        db_access(user_id, task_name, time)
 
     else:
-        speech_output = "もう一度時間をはっきりと話してみてください。" \
-                        "例えば　「4時間」　というふうに話してみてください"
-        reprompt_text = "もう一度話してみてください"
+        speech_output = u"もう一度時間をはっきりと話してみてください。" \
+                        u"例えば　「4時間」　というふうに話してみてください"
+        reprompt_text = u"もう一度話してみてください"
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def get_time_from_json(intent, session):
+def get_time_by_date(intent, session):
     session_attributes = {}
     reprompt_text = None
 
@@ -295,27 +285,23 @@ def get_time_from_json(intent, session):
             intent['slots'].get("date", {}) and
             intent['slots'].get("target", {})):
 
-        # get the slots
-        slots  = intent['slots']
-        date   = slots['date']['value']
+        # get from slots
+        slots = intent['slots']
+        date = slots['date']['value']
         target = slots['target']['value']
+        user_id = session['user']['userId']
 
-        # task_name = session['attributes']['taskName']
-        # time     = session['attributes']['time']
-        print("##### show iso date")
-        print(type(date))
-        print(date)
-
-        time = get_time_by_target(target, date)
+        time = get_time_by_target(user_id, target, date)
+        # 日にちのデータが週でないとき
         if date.find("W") == -1:
-            speech_output = date + "の" + target + "に使用した時間は" + str(time) + "時間です。"
+            speech_output = date + u"の" + target + u"に使用した時間は" + str(time) + u"時間です。"
+        # TODO 週の判定
         else:
-            speech_output = "今週の" + target + "に使用した時間は" + str(time) + "時間です。"
-
+            speech_output = u"今週の" + target + u"に使用した時間は" + str(time) + u"時間です。"
         should_end_session = True
 
     else:
-        speech_output = "もう一度話してみてください。"
+        speech_output = u"もう一度話してみてください。"
         should_end_session = False
 
     return build_response(session_attributes, build_speechlet_response(
@@ -324,22 +310,14 @@ def get_time_from_json(intent, session):
 
 # --------------- Events ------------------
 
-def on_session_started(session_started_request, session):
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
+def on_launch(session):
+    print("on_launch userId=" + session['user']['userId'])
 
-
-def on_launch(launch_request, session):
-    print("on_launch requestId=" + launch_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
     # Dispatch to your skill's launch
     return get_welcome_response()
 
 
 def on_intent(intent_request, session):
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
     print('intent_name ===' + intent_name)
@@ -352,7 +330,7 @@ def on_intent(intent_request, session):
         return set_time_in_session(intent, session)
     # 今日のゆーちゅーぶの時間を教えて
     elif intent_name == "GetTimeIntent":
-        return get_time_from_json(intent, session)
+        return get_time_by_date(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -361,7 +339,10 @@ def on_intent(intent_request, session):
         raise ValueError("Invalid intent")
 
 
-def on_session_ended(session_ended_request, session):
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+def on_session_started(session):
+    print("on_session_started userId=" + session['user']['userId'])
+
+
+def on_session_ended(session):
+    print("on_session_ended userId=" + session['user']['userId'])
     # add cleanup logic here
